c1b2e5fba8d23717c3ad070a3f1883cce312943f
kotomasakashiwagi <73920282+kotomasakashiwagi@users.noreply.github.com>
2020-11-04 18:40:21 +0900

    1-1完了

---
+++ src/main/java/HelloWorldPrinter.java
@@ -0,0 +1,6 @@
+public class HelloWorldPrinter {
+    private static final String WORD = "hello world";
+    public static void printWord(){
+        System.out.println(WORD);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,6 @@
 public class Main {
-    public static void main(String[] args){
-        if(args.length < 1){
-            System.err.println("引数にメール");
-            System.exit(-1);
-        }
-        String input = args[0];
-        String input1 = args[1];
-        System.out.println(input1);
-        int begin = input.indexOf("<");
-        if (begin >= 0){
-            begin++;
-            int end = input.indexOf(">");
-            if(end>begin){
-                String addr = input.substring(begin,end);
-                System.out.println(addr);
-            }
-        }
-        //System.out.println(s.contains("1"));
+    public static void main(String[] args) {
+        HelloWorldPrinter.printWord();
     }
 }
+
