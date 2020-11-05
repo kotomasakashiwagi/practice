c3e1164171f75e680be8c311d9c2ee377feaff9f
kotomasakashiwagi <73920282+kotomasakashiwagi@users.noreply.github.com>
2020-11-05 11:01:42 +0900

    1-3 Done

--- src/main/java/IntPrinter.java
+++ src/main/java/IntPrinter.java
@@ -1,6 +1,13 @@
 public class IntPrinter {
-    private static int NUMBER = 11;
+    private static int NUMBER_1 = 11;
+    private static int NUMBER_X = 13;
+    private static int NUMBER_Y = 17;
     public static void printInt(){
-        System.out.println("\"x="+NUMBER+"\"");
+        int x = NUMBER_1;
+        System.out.println("\"x="+x+"\"");
     }
+    public static void printIntPair(){
+        System.out.println("\"x="+NUMBER_X+","+"\"y="+NUMBER_Y+"\"");
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        //HelloWorldPrinter.printWord();
-        IntPrinter.printInt();
+        if(args[0].equals("1-1")){
+            HelloWorldPrinter.printWord();
+        }
+        if(args[0].equals("1-2")){
+            IntPrinter.printInt();
+        }
+        if(args[0].equals("1-3")){
+           IntPrinter.printIntPair();
+        }
+        if(args[0].equals("1-4")){
+            IntPrinter.printInt();
+        }
+        if(args[0].equals("1-5")){
+            IntPrinter.printIntPair();
+        }
+        if(args[0].equals("1-6")){
+            IntPrinter.printIntPair();
+        }
+        if(args[0].equals("1-7")){
+            IntPrinter.printIntPair();
+        }
+        if(args[0].equals("1-8")){
+            IntPrinter.printIntPair();
+        }
+        if(args[0].equals("1-9")){
+            IntPrinter.printIntPair();
+        }
+        if(args[0].equals("1-10")){
+            IntPrinter.printIntPair();
+        }
+        if(args[0].equals("1-11")){
+            IntPrinter.printIntPair();
+        }
+        if(args[0].equals("1-12")) {
+            IntPrinter.printIntPair();
+        }
     }
 }
+
 
